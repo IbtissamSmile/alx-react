366f743c0e21040420d31e5c9a6ceeda52ef6cc0
Ibtissam Toujni <122944193+IbtissamSmile@users.noreply.github.com>
2024-06-07 08:16:20 -0700

    Delete 0x08-react_redux_reducer_selector directory

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/courses.js
+++
@@ -1,8 +0,0 @@
-import { schema, normalize } from 'normalizr';
-
-const courseSchema = new schema.Entity('courses');
-
-export function coursesNormalizer(data) {
-  const normalizedData = normalize(data, [courseSchema]);
-  return normalizedData;
-}
